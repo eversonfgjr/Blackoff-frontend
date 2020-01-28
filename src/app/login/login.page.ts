e33c50545fc3adfcb1736d77b2b751ef74c99f92
Everson Junior <eversonfgjr@gmail.com>
2020-01-28 14:55:05 -0200

    Prototipo da tela inicial desenvolvida - falta melhorar o CSS

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  nomeUsuario: string;
+  senha: string;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login(){
+
+  }
+
+  cadastrar(){
+    
+  }
+
+
+}
