81a29113ca0d31537b0ad4aa207e03afe45c29ed
Everson Junior <eversonfgjr@gmail.com>
2020-02-03 15:24:08 -0200

    Adicionado mais itens ao menu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,19 +17,25 @@ export class HomePage {
   carregaItens(){
     this.items =  [
       {
-        titulo: 'tenis Nike',
+        titulo: 'Tênis Nike',
         preco: 500.00,
         imageUrl: 'https://static.zattini.com.br/produtos/tenis-nike-air-max-oketo-feminino/06/HZM-0646-306/HZM-0646-306_zoom1.jpg'
       },
       {
-        titulo: 'tenis Adidas',
+        titulo: 'Tênis Adidas',
         preco: 350.00,
         imageUrl: 'https://www.todamulher.com.br/wp-content/uploads/2018/06/52-1.jpg'
       },
       {
-        titulo: 'tenis Oxer',
+        titulo: 'Tênis Oxer',
         preco: 150.00,
         imageUrl: 'https://imgcentauro-a.akamaihd.net/900x900/9281762Y/tenis-oxer-velocity-masculino-img.jpg'
+      },
+      {
+        titulo: 'Tênis Mizuno Atlas Masculino - Cinza e Branco',
+        preco: 129.99,
+        // tslint:disable-next-line: max-line-length
+        imageUrl: 'http://t0.gstatic.com/images?q=tbn%3AANd9GcRElNTgHbXs-AaReUyFMvnLxKz3uDbl0C_8AAaDgfAQ90962zhgDPdU00kkI24QSaL5RZUHRhmxfw&usqp=CAc'
       }
     ];
   }
